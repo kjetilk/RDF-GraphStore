e83a64e621929dbbd4f9141aab4b4b2cee8e0016
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2010-06-26 23:26:24 +0200

    Use RDF::Query->error in array context for better error messages

--- lib/RDF/Query/HTTPBindings.pm
+++ lib/RDF/Query/HTTPBindings.pm
@@ -121,11 +121,7 @@ sub put_response {
     $sparql .= "INSERT DATA { GRAPH <$uri> {\n\t" . _serialize_payload( $new_model ) . '} }';
     $res->location($uri);
   }
-  my $query = RDF::Query->new($sparql, { update => 1 });
-  unless (defined($query)) {
-    my $err = RDF::Query->error;
-    confess $err;
-  }
+  my $query = RDF::Query->new($sparql, { update => 1 }) || confess (RDF::Query->error);
   $query->execute($self->model); # TODO: What could go wrong here and how do we deal with it?
   $res->code(201);
   return $res;
@@ -150,11 +146,7 @@ sub post_response {
   }
   # TODO: How do we escape the payload for security?
   my $sparql = "INSERT DATA { GRAPH <$uri> {\n\t" . _serialize_payload($add_model) . '} }';
-  my $query = RDF::Query->new($sparql, { update => 1 });
-  unless (defined($query)) {
-    my $err = RDF::Query->error;
-    confess $err;
-  }
+  my $query = RDF::Query->new($sparql, { update => 1 }) || confess (RDF::Query->error);
   $query->execute($self->model); # TODO: What could go wrong here and how do we deal with it?
   $res->code(204);
   # TODO: Support the "201 + Location" scenario
@@ -172,11 +164,7 @@ sub delete_response {
   my $uri = _check_uri(shift);
   my $res = Plack::Response->new;
   my $sparql = "CLEAR GRAPH <$uri>"; # DROP GRAPH is not implemented
-  my $query = RDF::Query->new($sparql, { update => 1 });
-  unless (defined($query)) {
-    my $err = RDF::Query->error;
-    confess $err;
-  }
+  my $query = RDF::Query->new($sparql, { update => 1 }) || confess (RDF::Query->error);
   $query->execute($self->model); # TODO: What could go wrong here and how do we deal with it?
   $res->code(204);
   return $res;
